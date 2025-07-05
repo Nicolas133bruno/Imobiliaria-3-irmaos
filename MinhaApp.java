09c23c645d83b0adedb4dd01b6a5254708dad242
Nicolas133bruno <161617913+Nicolas133bruno@users.noreply.github.com>
2025-07-05 00:20:28 -0300

    Add files via upload

---
+++ MinhaApp.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MinhaApp {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://" + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASS");
+
+        try {
+            // Carregar driver MySQL (opcional no Java 8+)
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Usuario LIMIT 3");
+
+            while(rs.next()){
+                System.out.println("Usuário: " + rs.getString("nome") + ", Email: " + rs.getString("email"));
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
