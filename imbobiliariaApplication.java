7936dc709557a2e12f23f7eb4a1a97cd8098d373
Nicolas133bruno <161617913+Nicolas133bruno@users.noreply.github.com>
2025-10-09 03:09:51 -0300

    Add files via upload

---
+++ imbobiliariaApplication.java
@@ -0,0 +1,9 @@
+package br.edu.iftm.ds.exemplos.imobiliaria;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class imbobiliariaApplication {
+    public static void main(String[] args){ SpringApplication.run(imbobiliariaApplication.class, args); }
+}
+
