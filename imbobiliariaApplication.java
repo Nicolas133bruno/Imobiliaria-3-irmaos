ad483586424c66d6c53b266006027501a4c9bf8f
Nicolas133bruno <161617913+Nicolas133bruno@users.noreply.github.com>
2025-10-10 11:19:01 -0300

    Delete imbobiliariaApplication.java

--- imbobiliariaApplication.java
+++
@@ -1,9 +0,0 @@
-package br.edu.iftm.ds.exemplos.imobiliaria;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class imbobiliariaApplication {
-    public static void main(String[] args){ SpringApplication.run(imbobiliariaApplication.class, args); }
-}
-
